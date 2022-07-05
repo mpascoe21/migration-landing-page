61260d4c284409f142b35fb91a09f9625e8de214
Maria <pascoe_maria@hotmail.com>
2022-07-05 09:32:05 +0100

    Desktop layout complete

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const howImg = document.querySelector(".how__img");
 const howText = document.querySelector(".how__text");
+const headerImg = document.querySelector(".home__img");
+const thinkAboutText = document.querySelector(".think-about__text");
 
 if (screen.width > 767) {
   howImg.attributes[1].nodeValue =
@@ -7,4 +9,14 @@ if (screen.width > 767) {
 
   howText.innerHTML =
     "We’ve helped lots of businesses switch to O2, so we know how to make the process as smooth as possible. The steps below provide the foundation for every project:";
+}
+
+if (screen.width >= 1920) {
+  headerImg.attributes[1].nodeValue = "./assets/images/img-header@3x.png";
+
+  thinkAboutText.innerHTML =
+    "Switching mobile provider might be the kind of thing you haven’t given a second thought to ― after all, you’re probably pretty busy with all the things that come with running a business. <br><br> But mobile matters. Especially in a constantly evolving workplace. So getting it right is important, and that’s a good reason to switch sooner.";
+
+  howImg.attributes[1].nodeValue =
+    "./assets/images/img-icons-path-desktop@3x.png";
 }
